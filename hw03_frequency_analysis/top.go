55c77b165df2c12c2b428278a276c2fb5a9abd46
sadbanzai <sadbanzai@gmail.com>
2023-10-18 22:45:46 +0500

    add top10 func

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,36 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(s string) []string {
+	splited := strings.Fields(s)
+	count := map[string]int{}
+	for _, word := range splited {
+		count[word]++
+	}
+	type pair struct {
+		Key   string
+		Value int
+	}
+	var pairs []pair
+	for key, val := range count {
+		pairs = append(pairs, pair{key, val})
+	}
+	sort.Slice(pairs, func(i, j int) bool {
+		if pairs[i].Value != pairs[j].Value {
+			return pairs[i].Value > pairs[j].Value
+		}
+		return pairs[i].Key < pairs[j].Key
+	})
+	var result []string
+	for _, val := range pairs {
+		result = append(result, val.Key)
+	}
+	if len(result) >= 10 {
+		return result[:10]
+	}
+	return result
 }
