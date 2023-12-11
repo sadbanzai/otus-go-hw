7fac2904cc8b396aa814973d5d7d10c2b8db1f33
sadbanzai <sadbanzai@gmail.com>
2023-12-11 22:02:17 +0500

    add copy func

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("unsupported file", func(t *testing.T) {
+		err := Copy("/dev/urandom", "out.txt", 0, 0)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("no permissions from-file", func(t *testing.T) {
+		err := Copy("/etc/sudoers", "out.txt", 0, 0)
+		require.ErrorIs(t, err, os.ErrPermission)
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "out.txt", 10000, 0)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("limit exceeds file size", func(t *testing.T) {
+		toFile, _ := os.CreateTemp("/tmp", "out")
+		err := Copy("testdata/input.txt", toFile.Name(), 0, 10000)
+		require.NoError(t, err)
+	})
 }
