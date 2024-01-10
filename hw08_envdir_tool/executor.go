a3ea2c71da99120ab640acb6bceb8982c666f50b
sadbanzai <sadbanzai@gmail.com>
2024-01-10 21:44:52 +0500

    add envdir

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,43 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
+var (
+	// UnknownErrorExitCode returns when cannot run child
+	UnknownErrorExitCode = 111
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for k, v := range env {
+		if v.NeedRemove {
+			err := os.Unsetenv(k)
+			if err != nil {
+				return UnknownErrorExitCode
+			}
+		}
+		err := os.Setenv(k, v.Value)
+		if err != nil {
+			return UnknownErrorExitCode
+		}
+	}
+
+	runCmd := exec.Command(cmd[0], cmd[1:]...)
+	runCmd.Env = os.Environ()
+	runCmd.Stdin = os.Stdin
+	runCmd.Stdout = os.Stdout
+	runCmd.Stderr = os.Stderr
+
+	err := runCmd.Run()
+	switch err := err.(type) {
+	case nil:
+		return 0
+	case *exec.ExitError:
+		return err.ExitCode()
+	default:
+		return UnknownErrorExitCode
+	}
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,23 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("exit code", func(t *testing.T) {
+		exitCode := RunCmd([]string{"sh", "-c", "exit 2"}, Environment{})
+		require.Equal(t, 2, exitCode)
+	})
+
+	t.Run("exit code with env", func(t *testing.T) {
+		exitCode := RunCmd([]string{"sh", "-c", "exit $EXITCODE"}, Environment{"EXITCODE": EnvValue{Value: "2", NeedRemove: false}})
+		require.Equal(t, 2, exitCode)
+	})
+
+	t.Run("unknown error exit code", func(t *testing.T) {
+		exitCode := RunCmd([]string{"abcd"}, Environment{})
+		require.Equal(t, UnknownErrorExitCode, exitCode)
+	})
 }
